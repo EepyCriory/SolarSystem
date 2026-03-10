6ca5bd5aa31dabb91baa4ce43557d7f6234cb0e5
EepyCriory <aop2005.contact@gmail.com>
2026-03-10 11:08:26 +0100

    Fixed Moon to orbit around the Earth my changing the starting position and herachy.
    Also fixed scales in that same scene.

--- Assets/Scripts/Earth2.cs
+++ Assets/Scripts/Earth2.cs
@@ -9,16 +9,14 @@ public class Earth2 : MonoBehaviour
     //Private Methods
     private float rotationSpeed = 0.986f;
     private float distanceSun = 3699.915f;
-    private float scale = 12.76f;
+    private float scale = 0.009115393f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         transform.position = new Vector3(distanceSun, 0, 0);
         transform.localScale = new Vector3(scale, scale, scale);
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.RotateAround(sun.position, Vector3.up, rotationSpeed * Time.deltaTime);
--- Assets/Scripts/Moon.cs
+++ Assets/Scripts/Moon.cs
@@ -7,17 +7,15 @@ public class Moon : MonoBehaviour
 
     //Private Methods
     private float rotationSpeed = 360f;
-    private float distanceEarth = 3713.983f;
-    private float scale = 3.474f;
+    private float distanceEarth = 14.068f;
+    private float scale = 0.2722571f;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        transform.position = new Vector3(distanceEarth, 0, 0);
+        transform.position = earth.position + new Vector3(distanceEarth, 0, 0);
         transform.localScale = new Vector3(scale, scale, scale);
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.RotateAround(earth.position, Vector3.up, rotationSpeed * Time.deltaTime);
